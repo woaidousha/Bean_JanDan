8809ff1cb4acdb97d08222ac3c774d5c0b80584c
Liu Yulong <woaidousha@qq.com>
2015-05-04 11:13:36 +0800

    修改disk缓存路径

--- app/src/main/java/org/bean/jandan/BeanApp.java
+++ app/src/main/java/org/bean/jandan/BeanApp.java
@@ -2,7 +2,15 @@ package org.bean.jandan;
 
 import android.app.Application;
 
+import com.facebook.cache.disk.DiskCacheConfig;
+import com.facebook.common.internal.Supplier;
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.imagepipeline.core.ImagePipelineConfig;
+
+import org.bean.jandan.common.C;
+import org.bean.jandan.common.util.DebugLog;
+
+import java.io.File;
 
 /**
  * Created by liuyulong@yixin.im on 2015/4/30.
@@ -12,6 +20,26 @@ public class BeanApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Fresco.initialize(this);
+        DebugLog.d(BeanApp.this.getExternalCacheDir().getAbsolutePath());
+        initFresco();
+    }
+
+    public void initFresco() {
+        ImagePipelineConfig.Builder configBuilder = ImagePipelineConfig.newBuilder(this);
+        DiskCacheConfig.Builder diskCacheConfig = DiskCacheConfig.newBuilder();
+        diskCacheConfig.setBaseDirectoryPathSupplier(new
+            Supplier<File>() {
+                @Override
+                public File get() {
+                    return BeanApp.this.getExternalCacheDir();
+                }
+            })
+        .setBaseDirectoryName(C.DISK_CACHE.CACHE_PATH)
+        .setMaxCacheSize(C.DISK_CACHE.MAX_CACHE_SIZE)
+        .setMaxCacheSizeOnLowDiskSpace(C.DISK_CACHE.MAX_CACHE_SIZE_ON_LOW_DISK_SPACE)
+        .setMaxCacheSizeOnVeryLowDiskSpace(C.DISK_CACHE.MAX_CACHE_SIZE_ON_VERY_LOW_DISK_SPACE);
+        configBuilder.setMainDiskCacheConfig(diskCacheConfig.build());
+        configBuilder.setMainDiskCacheConfig(diskCacheConfig.build());
+        Fresco.initialize(this, configBuilder.build());
     }
 }
--- app/src/main/java/org/bean/jandan/common/C.java
+++ app/src/main/java/org/bean/jandan/common/C.java
@@ -11,4 +11,14 @@ public class C {
         public static final String OOXX = "http://jandan.net/?oxwlxojflwblxbsapi=jandan.get_ooxx_comments";
     }
 
+    public static interface UNIT {
+        public static final int MB = 1024 * 1024;
+    }
+
+    public static interface DISK_CACHE {
+        public static final String CACHE_PATH = "image_cache";
+        public static final int MAX_CACHE_SIZE = 300 * UNIT.MB;
+        public static final int MAX_CACHE_SIZE_ON_LOW_DISK_SPACE = 100 * UNIT.MB;
+        public static final int MAX_CACHE_SIZE_ON_VERY_LOW_DISK_SPACE = 50 * UNIT.MB;
+    }
 }
