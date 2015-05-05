2aac89b57469c58299674e4fd5a384cd024516d0
Liu Yulong <woaidousha@qq.com>
2015-05-05 18:36:20 +0800

    update launcher icon

--- app/src/main/java/org/bean/jandan/common/C.java
+++ app/src/main/java/org/bean/jandan/common/C.java
@@ -5,27 +5,27 @@ package org.bean.jandan.common;
  */
 public interface C {
 
-    public static interface URL {
+    interface URL {
         /*无聊图*/
-        public static final String PIC = "http://jandan.net/?oxwlxojflwblxbsapi=jandan.get_pic_comments";
-        public static final String OOXX = "http://jandan.net/?oxwlxojflwblxbsapi=jandan.get_ooxx_comments";
+        String PIC = "http://jandan.net/?oxwlxojflwblxbsapi=jandan.get_pic_comments";
+        String OOXX = "http://jandan.net/?oxwlxojflwblxbsapi=jandan.get_ooxx_comments";
 
-        public static final int FIRST_PAGE = 1;
-        public static final String PARAM_PAGE = "page";
+        int FIRST_PAGE = 1;
+        String PARAM_PAGE = "page";
     }
 
-    public static interface UNIT {
-        public static final int MB = 1024 * 1024;
+    interface UNIT {
+        int MB = 1024 * 1024;
     }
 
-    public static interface DISK_CACHE {
-        public static final String CACHE_PATH = "image_cache";
-        public static final int MAX_CACHE_SIZE = 300 * UNIT.MB;
-        public static final int MAX_CACHE_SIZE_ON_LOW_DISK_SPACE = 100 * UNIT.MB;
-        public static final int MAX_CACHE_SIZE_ON_VERY_LOW_DISK_SPACE = 50 * UNIT.MB;
+    interface DISK_CACHE {
+        String CACHE_PATH = "image_cache";
+        int MAX_CACHE_SIZE = 300 * UNIT.MB;
+        int MAX_CACHE_SIZE_ON_LOW_DISK_SPACE = 100 * UNIT.MB;
+        int MAX_CACHE_SIZE_ON_VERY_LOW_DISK_SPACE = 50 * UNIT.MB;
     }
 
-    public static enum Directory {
+    enum Directory {
 
         SHARE("share", false);
 
