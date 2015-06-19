09ef0a80ca9ef239e1e1daab5fe48a8c2e4c80c4
Liu Yulong <woaidousha@qq.com>
2015-06-19 11:13:34 +0800

    update UI

--- app/src/main/java/org/bean/jandan/adapter/helper/ImageFetchHelper.java
+++ app/src/main/java/org/bean/jandan/adapter/helper/ImageFetchHelper.java
@@ -1,15 +1,20 @@
 package org.bean.jandan.adapter.helper;
 
 import android.graphics.Bitmap;
+import android.graphics.drawable.Animatable;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.text.TextUtils;
 import android.widget.ImageView;
 
 import com.facebook.common.executors.HandlerExecutorServiceImpl;
 import com.facebook.common.references.CloseableReference;
 import com.facebook.datasource.DataSource;
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.backends.pipeline.PipelineDraweeController;
+import com.facebook.drawee.controller.AbstractDraweeController;
+import com.facebook.drawee.controller.BaseControllerListener;
 import com.facebook.drawee.drawable.ScalingUtils;
 import com.facebook.drawee.generic.GenericDraweeHierarchy;
 import com.facebook.drawee.generic.GenericDraweeHierarchyBuilder;
@@ -19,6 +24,7 @@ import com.facebook.imagepipeline.common.Priority;
 import com.facebook.imagepipeline.core.ImagePipeline;
 import com.facebook.imagepipeline.datasource.BaseBitmapDataSubscriber;
 import com.facebook.imagepipeline.image.CloseableImage;
+import com.facebook.imagepipeline.image.ImageInfo;
 import com.facebook.imagepipeline.request.ImageRequest;
 import com.facebook.imagepipeline.request.ImageRequestBuilder;
 
@@ -40,7 +46,7 @@ public class ImageFetchHelper {
         mExecutorService = new HandlerExecutorServiceImpl(new Handler(sBitmapLoadThread.getLooper()));
     }
 
-    public static void fetchRecyclerViewImage(SimpleDraweeView view, Uri uri, boolean withProgress) {
+    public static void fetchRecyclerViewImage(final SimpleDraweeView view, Uri uri, boolean withProgress) {
         if (view == null) {
             return;
         }
@@ -53,8 +59,12 @@ public class ImageFetchHelper {
                                             .setImageRequest(request)
                                             .setAutoPlayAnimations(true)
                                             .setOldController(view.getController())
+                                            .setControllerListener(new AspectRatioControllerListener(view))
                                             .build();
         view.setController(controller);
+        if (PipelineDraweeController.class.isInstance(controller)) {
+            view.setTag(((AbstractDraweeController) controller).getId());
+        }
         if (view.getHierarchy() != null) {
             ImageProgressBarDrawable drawable = null;
             if (withProgress) {
@@ -128,5 +138,32 @@ public class ImageFetchHelper {
 
     public static Uri buildStringUri(String url) {
         return Uri.parse(url);
+    }
+
+    static class AspectRatioControllerListener extends BaseControllerListener<ImageInfo> {
+
+        private SimpleDraweeView view;
+
+        public AspectRatioControllerListener(SimpleDraweeView view) {
+            this.view = view;
+        }
+
+        @Override
+        public void onFinalImageSet(String id, ImageInfo imageInfo, Animatable animatable) {
+            setAspectRatio(id, imageInfo);
+        }
+
+        @Override
+        public void onIntermediateImageSet(String id, ImageInfo imageInfo) {
+            setAspectRatio(id, imageInfo);
+        }
+
+        private void setAspectRatio(String id, ImageInfo imageInfo) {
+            float ratio = 1.0f * imageInfo.getWidth() / imageInfo.getHeight();
+            Object tag = view.getTag();
+            if (String.class.isInstance(tag) && TextUtils.equals((String) tag, id)) {
+                view.setAspectRatio(ratio);
+            }
+        }
     }
 }
